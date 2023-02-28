385a33076eb5a6e3daaafc45e642822962053867
Prerana chavan <99093399+pranu1611@users.noreply.github.com>
2023-02-28 13:13:49 +0530

    first commit

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+  {
+  numb: 1,
+  question: "What does the abbreviation HTML stand for?",
+  answer: "Hyper Text Markup Language",
+  options: [
+    "Hyper Text Preprocessor",
+    "Hyper Text Markup Language",
+    "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+  ]
+},
+  {
+  numb: 2,
+  question: "What does CSS stand for?",
+  answer: "Cascading Style Sheet",
+  options: [
+    "Common Style Sheet",
+    "Colorful Style Sheet",
+    "Computer Style Sheet",
+    "Cascading Style Sheet"
+  ]
+},
+  {
+  numb: 3,
+  question: "What does PHP stand for?",
+  answer: "Hypertext Preprocessor",
+  options: [
+    "Hypertext Preprocessor",
+    "Hypertext Programming",
+    "Hypertext Preprogramming",
+    "Hometext Preprocessor"
+  ]
+},
+  {
+  numb: 4,
+  question: "What does SQL stand for?",
+  answer: "Structured Query Language",
+  options: [
+    "Stylish Question Language",
+    "Stylesheet Query Language",
+    "Statement Question Language",
+    "Structured Query Language"
+  ]
+},
+  {
+  numb: 5,
+  question: "What does XML stand for?",
+  answer: "eXtensible Markup Language",
+  options: [
+    "eXtensible Markup Language",
+    "eXecutable Multiple Language",
+    "eXTra Multi-Program Language",
+    "eXamine Multiple Language"
+  ]
+},
+ {
+  numb: 6,
+  question: "What is the smallest header in HTML by default?",
+  answer: "h6",
+  options: [
+    "h1",
+    "h2",
+    "h6",
+    "h4"
+  ]
+},
+ {
+  numb: 7,
+  question: "How to set a font for a whole page?",
+  answer: "<defaultfont>",
+  options: [
+    "<targetfont>",
+    "<defaultfont>",
+    "<font>",
+    "None of the above"
+  ]
+},
+ {
+  numb: 8,
+  question: "The CSS inside HTML elements used alongside style attribute is called?",
+  answer: "Inline",
+  options: [
+    "Inline",
+    "external CSS",
+    "internal CSS",
+    "None of the above"
+  ]
+},
+ {
+  numb: 9,
+  question: "What are the main components of the front end of any working website?",
+  answer: "HTML,CSS,Javascript",
+  options: [
+    "HTML,CSS,Javascript",
+    "HTML only",
+    "CSS ONLY",
+    "Node.js"
+  ]
+},
+ {
+  numb: 10,
+  question: "Which of the following are closures in Javascript?",
+  answer: "All of the above",
+  options: [
+    "variables",
+    "functions",
+    "objects",
+    "All of the above"
+  ]
+},
+];
